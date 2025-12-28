c15d634e1f04ad3eafcf82ad756f2971b628af16
chenzhichao <zchen1@a4x.io>
2025-12-28 20:31:02 +0800

    feat: 测试部署

--- services/user-service/initialize/db.go
+++ services/user-service/initialize/db.go
@@ -34,7 +34,7 @@ func InitDB(dsn string) *gorm.DB {
 
 	// 自动迁移
 	if err := model.AutoMigrate(db); err != nil {
-		log.Printf("warning: failed to migrate database: %v", err)
+		log.Printf("warning: failed to migrate database: %v", err) // 改为普通log输出，否则zeabur会报错
 	}
 
 	// 初始化DAO
